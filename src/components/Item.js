06d350661e2b233c7ca39208c10c7423a65138a8
Max <voronin223@gmail.com>
2022-10-22 11:30:17 +0300

    eShop

---
+++ src/components/Item.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export class Items extends Component {
+    render() {
+        return (
+        <div className='item'>
+            <img src={"./img/" + this.props.item.img } />
+            <h2>{this.props.item.title}</h2>
+            <p>{this.props.item.category}</p>
+            <b>{this.props.item.price}$</b>
+            <h2>{this.props.item.desc}</h2>
+            <div className='add-to-cart' onClick={() => this.props.onAdd(this.props.item)}>+</div>
+
+        </div>
+        )
+    }
+}
+
+export default Items
